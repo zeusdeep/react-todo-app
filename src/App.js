c04c11bfe361edf2119d4344441d79cc69359ed0
Deep <deepjparekh@gmail.com>
2020-08-30 18:55:06 -0400

    useState react hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Task 1', 'Task 2']);
+
+
   return (
     <div className="App">
       <h1>Hello world</h1>
       <input />
+      
       <button> Add Todo </button>
+
+      <ul>
+        { todos.map(todo => (
+          <li> {todo} </li>
+          )
+        ) }
+      </ul>
     </div>
   );
 }

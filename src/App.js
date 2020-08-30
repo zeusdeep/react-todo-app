457961c53e2518984a9826095a05ff535dbec9ef
Deep <deepjparekh@gmail.com>
2020-08-30 19:20:14 -0400

    updating local state hooks

--- src/App.js
+++ src/App.js
@@ -2,21 +2,42 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState(['Task 1', 'Task 2']);
+  
+  // local state to hold tasks
+  const [todos, setTodos] = useState(
+    [
+      'Task 1', 
+      'Task 2', 
+      'Task 3'
+    ]);
 
+  const [inputTodo, setInputTodo] = useState('');
+
+  const addTodo = (event) => {
+    // function which fires off when input button tapped
+    console.log("addTodo", event.target.value)
+    // pushes inpputted todo to end of todos array
+    setTodos([...todos, inputTodo]);
+  }
 
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <input />
       
-      <button> Add Todo </button>
+      <input 
+        value = { inputTodo }
+        onChange = { event => setInputTodo(event.target.value) }
+      />
+      
+      <button onClick = {addTodo}> Add Todo </button>
 
       <ul>
-        { todos.map(todo => (
-          <li> {todo} </li>
-          )
-        ) }
+        { todos.map(
+          todo => ( 
+            <li> {todo} </li>
+            )
+          ) 
+        }
       </ul>
     </div>
   );

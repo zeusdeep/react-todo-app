51ac212aed26dcb5f61d6d73db77537af1ba62a4
Deep <deepjparekh@gmail.com>
2020-08-30 23:09:45 -0400

    delete and likes button working

--- src/Todo.js
+++ src/Todo.js
@@ -3,6 +3,8 @@ import { ListItem, ListItemText, List } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import './Todo.css';
+import db from './firebase';
+import firebase from "firebase";
 
 function Todo(props) {
     return (
@@ -14,10 +16,14 @@ function Todo(props) {
                 />
             </ListItem>
             <DeleteIcon  
-                //onClick = { event => }
+                onClick = { event => db.collection('todos').doc(props.todo.id).delete() }
             />
             <ThumbUpIcon  
-                //onClick = { event => }
+                onClick = { event => db.collection('todos').doc(props.todo.id).update({
+                    likes:props.todo.likes + 1,
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                })
+            }
             />
         </List>
     )
